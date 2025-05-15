9a8d8636320e692b44904edb660d661262ec51b6
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-05-15 19:19:13 +0300

    feat: add onboarding navigation with page controller and next button

--- lib/pages/welcome/widgets.dart
+++ lib/pages/welcome/widgets.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:ulearning_frontend/common/widgets/app_shadow.dart';
 import 'package:ulearning_frontend/common/widgets/text_widgets.dart';
 
-Widget appOnboardingPage({
+Widget appOnboardingPage(
+  PageController controller, {
   String imagePath = "assets/images/reading.png",
   String title = "",
   String subTitle = "",
+  int index = 0,
 }) {
   return Column(
     children: [
@@ -21,16 +23,28 @@ Widget appOnboardingPage({
         padding: EdgeInsets.only(left: 30, right: 30),
         child: text16Normal(text: subTitle),
       ),
-      _nextButton(),
+      _nextButton(index, controller),
     ],
   );
 }
 
-Widget _nextButton() {
-  return Container(
-    width: 325,
-    height: 50,
-    margin: EdgeInsets.only(top: 100, left: 25, right: 25),
-    decoration: appBoxShadow(),
+Widget _nextButton(int index, PageController controller) {
+  return GestureDetector(
+    onTap: () {
+      if (index < 3) {
+        controller.animateToPage(
+          index,
+          duration: Duration(milliseconds: 300),
+          curve: Curves.bounceIn,
+        );
+      }
+    },
+    child: Container(
+      width: 325,
+      height: 50,
+      margin: EdgeInsets.only(top: 100, left: 25, right: 25),
+      decoration: appBoxShadow(),
+      child: Center(child: text16Normal(text: "next", color: Colors.white)),
+    ),
   );
 }
