3bc2de36968b52c9cf243f5748ae297679941123
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-05-15 18:37:44 +0300

    refactor: add color parameter with default AppColors to text widgets

--- lib/common/widgets/text_widgets.dart
+++ lib/common/widgets/text_widgets.dart
@@ -1,27 +1,23 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ulearning_frontend/common/utils/app_colors.dart';
 
 // Inside the braces is called optional named Parameter
-Widget text24Normal({String text = ""}) {
+Widget text24Normal({String text = "", Color color = AppColors.primaryText}) {
   return Text(
     text,
     textAlign: TextAlign.center,
-    style: const TextStyle(
-      color: Colors.blue,
-      fontSize: 24,
-      fontWeight: FontWeight.normal,
-    ),
+    style: TextStyle(color: color, fontSize: 24, fontWeight: FontWeight.normal),
   );
 }
 
-Widget text16Normal({String text = ""}) {
+Widget text16Normal({
+  String text = "",
+  Color color = AppColors.primarySecondaryElementText,
+}) {
   return Text(
     text,
     textAlign: TextAlign.center,
-    style: const TextStyle(
-      color: Colors.blue,
-      fontSize: 16,
-      fontWeight: FontWeight.normal,
-    ),
+    style: TextStyle(color: color, fontSize: 16, fontWeight: FontWeight.normal),
   );
 }
