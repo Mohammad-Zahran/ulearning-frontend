5e6caa6baac379ca967deaf0fb10da7645ed2d46
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-05-16 19:44:13 +0300

    chore: move welcome_notifier files into welcome folder for better structure

--- lib/pages/welcome/welcome.dart
+++ lib/pages/welcome/welcome.dart
@@ -1,7 +1,7 @@
 import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:ulearning_frontend/pages/notifier/welcome_notifier.dart';
+import 'package:ulearning_frontend/pages/welcome/notifier/welcome_notifier.dart';
 import 'package:ulearning_frontend/pages/welcome/widgets.dart';
 
 class Welcome extends ConsumerWidget {
