830aa174ab181b76efea201beb94b2bd1d28d9ca
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-05-16 18:11:47 +0300

    feat: create IndexDotProvider with riverpod code generation

---
+++ lib/pages/notifier/welcome_notifier.dart
@@ -0,0 +1,10 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+part 'welcome_notifier.g.dart';
+
+@riverpod
+class IndexDotProvider extends _$IndexDotProvider {
+  @override
+  int build() {
+    return 0;
+  }
+}
