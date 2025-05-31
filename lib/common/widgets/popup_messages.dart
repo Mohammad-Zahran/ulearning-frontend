09f7e16118a128a2b8decc92743c4cd600174656
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-05-31 18:42:31 +0300

    feat: enhance toastInfo utility with customizable background and text color options

--- lib/common/widgets/popup_messages.dart
+++ lib/common/widgets/popup_messages.dart
@@ -1,6 +1,18 @@
+import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
-// positional optional parameter with default value
-toastInfo([String msg = ""]) {
-  return Fluttertoast.showToast(msg: msg);
+toastInfo(
+  String msg, {
+  Color backgroundColor = Colors.blue,
+  Color textColor = Colors.white,
+}) {
+  return Fluttertoast.showToast(
+    msg: msg,
+    toastLength: Toast.LENGTH_SHORT,
+    gravity: ToastGravity.TOP,
+    timeInSecForIosWeb: 2,
+    backgroundColor: backgroundColor,
+    textColor: textColor,
+    fontSize: 16.0,
+  );
 }
