8791d2e7805a782a42d21ff2817e0be4888b0991
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-06-01 13:16:04 +0300

    feat: use global AppLoader provider to display CircularProgressIndicator during sign up process

--- lib/pages/sign_up/sign_up.dart
+++ lib/pages/sign_up/sign_up.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ulearning_frontend/common/utils/app_colors.dart';
+import 'package:ulearning_frontend/common/utils/global_loader/global_loader.dart';
 import 'package:ulearning_frontend/common/widgets/app_bar.dart';
 import 'package:ulearning_frontend/common/widgets/app_textfields.dart';
 import 'package:ulearning_frontend/common/widgets/button_widgets.dart';
@@ -15,96 +17,105 @@ class SignUp extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     // Using the registerNotifierProvider to access the state and methods of RegisterNotifier
     final regProvider = ref.watch(registerNotifierProvider);
-    // regProvider.
+    final loader = ref.watch(appLoaderProvider);
+
     return Container(
       child: SafeArea(
         child: Scaffold(
           appBar: buildAppbar(title: "Sign Up"),
           backgroundColor: Colors.white,
-          body: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(height: 30),
-                // more login options can be added here
-                Center(
-                  child: text14Normal(
-                    text: "Enter your details below & free sign up",
+          body:
+              loader == false
+                  ? SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(height: 30),
+                        // more login options can be added here
+                        Center(
+                          child: text14Normal(
+                            text: "Enter your details below & free sign up",
+                          ),
+                        ),
+                        SizedBox(height: 50),
+                        // user name text box
+                        appTextField(
+                          text: "User name",
+                          iconName: "assets/icons/user.png",
+                          hintText: "Enter your user name",
+                          obscureText: false,
+                          // Using the notifier to handle user name changes
+                          func:
+                              (value) => ref
+                                  .read(registerNotifierProvider.notifier)
+                                  .onUserNameChange(value),
+                        ),
+                        SizedBox(height: 20),
+                        // email text box
+                        appTextField(
+                          text: "Email",
+                          iconName: "assets/icons/user.png",
+                          hintText: "Enter your email address",
+                          obscureText: false,
+                          func:
+                              (value) => ref
+                                  .read(registerNotifierProvider.notifier)
+                                  .onEmailChange(value),
+                        ),
+                        SizedBox(height: 20),
+                        // password text box
+                        appTextField(
+                          text: "Password",
+                          iconName: "assets/icons/lock.png",
+                          hintText: "Enter your Password",
+                          obscureText: true,
+                          func:
+                              (value) => ref
+                                  .read(registerNotifierProvider.notifier)
+                                  .onPasswordChange(value),
+                        ),
+                        SizedBox(height: 20),
+                        // confirm password text box
+                        appTextField(
+                          text: "Confirm Password",
+                          iconName: "assets/icons/lock.png",
+                          hintText: "Enter your Confirm Password",
+                          obscureText: true,
+                          func:
+                              (value) => ref
+                                  .read(registerNotifierProvider.notifier)
+                                  .onRePasswordChange(value),
+                        ),
+                        SizedBox(height: 20),
+                        // terms and conditions text
+                        Container(
+                          margin: EdgeInsets.only(right: 50, left: 50),
+                          child: text14Normal(
+                            text:
+                                "By creating an account, you agree to our Terms of Service and Privacy Policy.",
+                          ),
+                        ),
+                        SizedBox(height: 100),
+                        // app register button
+                        Center(
+                          child: appButton(
+                            buttonName: "Register",
+                            isLogin: true,
+                            context: context,
+                            func: () {
+                              SignUpController(ref: ref).handleSignUp();
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                  : Center(
+                    child: CircularProgressIndicator(
+                      backgroundColor: Colors.blue,
+                      color: AppColors.primaryElement,
+                    ),
                   ),
-                ),
-                SizedBox(height: 50),
-                // user name text box
-                appTextField(
-                  text: "User name",
-                  iconName: "assets/icons/user.png",
-                  hintText: "Enter your user name",
-                  obscureText: false,
-                  // Using the notifier to handle user name changes
-                  func:
-                      (value) => ref
-                          .read(registerNotifierProvider.notifier)
-                          .onUserNameChange(value),
-                ),
-                SizedBox(height: 20),
-                // email text box
-                appTextField(
-                  text: "Email",
-                  iconName: "assets/icons/user.png",
-                  hintText: "Enter your email address",
-                  obscureText: false,
-                  func:
-                      (value) => ref
-                          .read(registerNotifierProvider.notifier)
-                          .onEmailChange(value),
-                ),
-                SizedBox(height: 20),
-                // password text box
-                appTextField(
-                  text: "Password",
-                  iconName: "assets/icons/lock.png",
-                  hintText: "Enter your Password",
-                  obscureText: true,
-                  func:
-                      (value) => ref
-                          .read(registerNotifierProvider.notifier)
-                          .onPasswordChange(value),
-                ),
-                SizedBox(height: 20),
-                // confirm password text box
-                appTextField(
-                  text: "Confirm Password",
-                  iconName: "assets/icons/lock.png",
-                  hintText: "Enter your Confirm Password",
-                  obscureText: true,
-                  func:
-                      (value) => ref
-                          .read(registerNotifierProvider.notifier)
-                          .onRePasswordChange(value),
-                ),
-                SizedBox(height: 20),
-                // terms and conditions text
-                Container(
-                  margin: EdgeInsets.only(right: 50, left: 50),
-                  child: text14Normal(
-                    text:
-                        "By creating an account, you agree to our Terms of Service and Privacy Policy.",
-                  ),
-                ),
-                SizedBox(height: 100),
-                // app register button
-                Center(
-                  child: appButton(
-                    buttonName: "Register",
-                    isLogin: true,
-                    context: context,
-                    func: () {
-                      SignUpController(ref: ref).handleSignUp();
-                    },
-                  ),
-                ),
-              ],
-            ),
-          ),
         ),
       ),
     );
