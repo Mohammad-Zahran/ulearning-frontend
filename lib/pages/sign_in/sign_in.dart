c836cc73b79f17771b92916f0b22f9e004874315
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-06-02 15:01:45 +0300

    refactor: migrate sign_in.dart to ConsumerStatefulWidget and prepare for Riverpod-based state management

--- lib/pages/sign_in/sign_in.dart
+++ lib/pages/sign_in/sign_in.dart
@@ -1,69 +1,75 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ulearning_frontend/common/widgets/app_bar.dart';
 import 'package:ulearning_frontend/common/widgets/app_textfields.dart';
 import 'package:ulearning_frontend/common/widgets/button_widgets.dart';
 import 'package:ulearning_frontend/common/widgets/text_widgets.dart';
+import 'package:ulearning_frontend/pages/sign_in/notifier/sign_in_notifier.dart';
 import 'package:ulearning_frontend/pages/sign_in/widgets/sign_in_widgets.dart';
 
-class SignIn extends StatelessWidget {
+class SignIn extends ConsumerStatefulWidget {
   const SignIn({super.key});
 
   @override
+  ConsumerState<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends ConsumerState<SignIn> {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: SafeArea(
-        child: Scaffold(
-          appBar: buildAppbar(title: "Login"),
-          backgroundColor: Colors.white,
-          body: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                thirdPartyLogin(),
-                // more login options can be added here
-                Center(
-                  child: text14Normal(
-                    text: "Or use your email email account to login",
-                  ),
+    // To access notifier, use .notifier, to access state, just use provider
+    final signInNotifier = ref.watch(signInNotifierProvider.notifier);
+    return SafeArea(
+      child: Scaffold(
+        appBar: buildAppbar(title: "Login"),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              thirdPartyLogin(),
+              // more login options can be added here
+              Center(
+                child: text14Normal(
+                  text: "Or use your email email account to login",
                 ),
-                SizedBox(height: 50),
-                // email text box
-                appTextField(
-                  text: "Email",
-                  iconName: "assets/icons/user.png",
-                  hintText: "Enter your email address",
-                  obscureText: false,
+              ),
+              SizedBox(height: 50),
+              // email text box
+              appTextField(
+                text: "Email",
+                iconName: "assets/icons/user.png",
+                hintText: "Enter your email address",
+                obscureText: false,
+              ),
+              SizedBox(height: 20),
+              // password text box
+              appTextField(
+                text: "Password",
+                iconName: "assets/icons/lock.png",
+                hintText: "Enter your password",
+                obscureText: true,
+              ),
+              SizedBox(height: 20),
+              // forget text
+              Container(
+                margin: EdgeInsets.only(right: 200),
+                child: textUnderline(text: "Forget Password?"),
+              ),
+              SizedBox(height: 100),
+              // app login button
+              Center(child: appButton(buttonName: "Login", isLogin: true)),
+              SizedBox(height: 20),
+              // app register button
+              Center(
+                child: appButton(
+                  buttonName: "Register",
+                  isLogin: false,
+                  context: context,
+                  func: () => Navigator.pushNamed(context, "/register"),
                 ),
-                SizedBox(height: 20),
-                // password text box
-                appTextField(
-                  text: "Password",
-                  iconName: "assets/icons/lock.png",
-                  hintText: "Enter your password",
-                  obscureText: true,
-                ),
-                SizedBox(height: 20),
-                // forget text
-                Container(
-                  margin: EdgeInsets.only(right: 200),
-                  child: textUnderline(text: "Forget Password?"),
-                ),
-                SizedBox(height: 100),
-                // app login button
-                Center(child: appButton(buttonName: "Login", isLogin: true)),
-                SizedBox(height: 20),
-                // app register button
-                Center(
-                  child: appButton(
-                    buttonName: "Register",
-                    isLogin: false,
-                    context: context,
-                    func: () => Navigator.pushNamed(context, "/register"),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
